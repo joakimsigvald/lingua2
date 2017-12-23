32e4980b692d86a5c14bd136d99b2c4fb62cca03
joakimsvaltech <joakim.sigvald@valtech.se>
2017-12-23 13:18:08 +0100

    More efficient comparison of evaluations

--- Grammar/Evaluation.cs
+++ Grammar/Evaluation.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 
 namespace Lingua.Grammar
 {
@@ -8,10 +9,30 @@ namespace Lingua.Grammar
 
     public class Evaluation : IEvaluation, IEquatable<IEvaluation>
     {
+        private readonly string _code;
+
         public Evaluation(IList<Scoring> scorings)
         {
             Score = scorings.Sum(s => s.TotalScore);
             Patterns = scorings.Select(s => s.Pattern).ToList();
+            _code = MakeCode(Score, Patterns);
+        }
+
+        private static string MakeCode(int score, IEnumerable<ushort[]> patterns)
+        {
+            var sb = new StringBuilder();
+            sb.Append((char)score);
+            int i = 1;
+            int j;
+            foreach (var pattern in patterns)
+            {
+                j = 0;
+                for (; j < pattern.Length - 1; j += 2)
+                    sb.Append((char) (pattern[j] + (pattern[j + 1] << 16)));
+                if (j < pattern.Length)
+                    sb[0] = (char) (pattern[j] << 16);
+            }
+            return sb.ToString();
         }
 
         public IList<ushort[]> Patterns { get; }
@@ -20,12 +41,12 @@ namespace Lingua.Grammar
         public override string ToString() => $"{string.Join(",", Patterns.Select(Encoder.Serialize))}:{Score}";
 
         public bool Equals(IEvaluation other)
-            => other != null && other.Patterns.SequenceEqual(Patterns) && other.Score == Score;
+            => other != null && ((Evaluation)other)._code == _code;
 
         public override bool Equals(object obj)
             => Equals(obj as IEvaluation);
 
         public override int GetHashCode()
-            => Patterns.Sum(p => p.Sum(c => c));
+            => _code.GetHashCode();
     }
 }
