cfc406c165a46658e3d330e430b9012cd08c7dfc
joakimsvaltech <joakim.sigvald@valtech.se>
2017-12-23 21:08:01 +0100

    optimized priority

--- Learning/ScoredPattern.cs
+++ Learning/ScoredPattern.cs
@@ -14,8 +14,8 @@ namespace Lingua.Learning
             Priority = ComputePriority();
         }
 
-        private double ComputePriority()
-            => ComputeSize() * Math.Sqrt(Math.Abs(Score)) * (Score < 0 ? 2 : 1);
+        private int ComputePriority()
+            => (int)(ComputeSize() * Math.Sqrt(Math.Abs(Score)) * (Score < 0 ? 2 : 1));
 
         private int ComputeSize()
             => Code.Sum(ComputeSize);
@@ -23,13 +23,14 @@ namespace Lingua.Learning
         private static int ComputeSize(ushort code)
         {
             var modifiers = code & Encoder.ProperModifiersMask;
-            var size = 2 + (int)Math.Pow(2, modifiers.CountBits());
-            return (code & Encoder.Wildcard) == 0 ? size : size - 2;
+            var bitCount = modifiers.CountBits();
+            var size = 3 + (int)Math.Pow(2, bitCount);
+            return (code & Encoder.Wildcard) == 0 ? size : size - 3;
         }
 
         public ushort[] Code{ get; }
         public string Pattern => Encoder.Serialize(Code);
         public sbyte Score { get; }
-        public double Priority { get; }
+        public int Priority { get; }
     }
 }
